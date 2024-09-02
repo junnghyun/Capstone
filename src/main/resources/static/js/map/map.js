8dbbe72328a451c36ddf406f455229ef7b04615d
안정현 <ajhajh1005@naver.com>
2024-09-02 16:12:36 +0900

    [Fix]지도 화면 수정

--- src/main/resources/static/js/map/map.js
+++ src/main/resources/static/js/map/map.js
@@ -1,45 +1,38 @@
-
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+var mapContainer = document.getElementById('map'),
     mapOption = {
-        center: new kakao.maps.LatLng(37.56587, 126.97668), // 기본 중심 좌표
-        level: 3, // 지도의 확대 레벨
-        mapTypeId: kakao.maps.MapTypeId.ROADMAP // 지도 종류
+        center: new kakao.maps.LatLng(36.911602, 127.12831),
+        level: 3,
+        mapTypeId: kakao.maps.MapTypeId.ROADMAP
     };
 
-// 지도 생성
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 지도 타입 변경 컨트롤 생성
 var mapTypeControl = new kakao.maps.MapTypeControl();
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-// 확대 축소 컨트롤 생성
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-// 사용자 현재 위치 가져와서 지도 중심으로 설정
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
-        var locPosition = new kakao.maps.LatLng(lat, lon), // 좌표 객체 생성
-            message = '<div style="padding:5px;">Current Location</div>'; // 정보 창 내용
+        var lat = position.coords.latitude,
+            lon = position.coords.longitude;
+        var locPosition = new kakao.maps.LatLng(lat, lon),
+            message = '<div style="padding:5px;">Current Location</div>';
 
-        map.setCenter(locPosition); // 지도의 중심을 현재 위치로 이동
+        map.setCenter(locPosition);
 
         var marker = new kakao.maps.Marker({
             map: map,
             position: locPosition
         });
 
-        // 정보창을 생성하고 지도에 표시합니다
         var infowindow = new kakao.maps.InfoWindow({
             content: message,
             removable: true
         });
         infowindow.open(map, marker);
 
-        // 마커에 클릭 이벤트 등록
         kakao.maps.event.addListener(marker, 'click', function() {
             infowindow.open(map, marker);
         });
@@ -48,20 +41,14 @@ if (navigator.geolocation) {
     alert("Unable to use your current location.");
 }
 
-// 추가 마커 생성 및 표시
-var markerPosition  = new kakao.maps.LatLng(37.56682, 126.97865);
-var marker2 = new kakao.maps.Marker({
-    position: markerPosition,
-    map: map
-});
+var markerPosition = new kakao.maps.LatLng(36.911602, 127.12831);
 
-// 도로 경로 설정
 var roadPath = [
-    new kakao.maps.LatLng(37.56635, 126.98006),
-    new kakao.maps.LatLng(37.56682, 126.97865)
+    new kakao.maps.LatLng(36.911602, 127.12831),
+    new kakao.maps.LatLng(36.911221, 127.12851)
 ];
 
-var content = '<img id="roadImage" src="/img/DJI_0049.JPG" alt="도로 이미지" style="position: absolute;">';
+var content = '<img id="roadImage" src="/Users/anjeonghyeon/Downloads/rotated_image.jpg" alt="도로 이미지" style="position: absolute;">';
 
 var customOverlay = new kakao.maps.CustomOverlay({
     map: map,
@@ -84,7 +71,6 @@ function adjustOverlaySize() {
 
     image.style.width = width + 'px';
     image.style.height = height + 'px';
-    customOverlay.setPosition(roadPath[0]);
 }
 
 kakao.maps.event.addListener(map, 'zoom_changed', adjustOverlaySize);
